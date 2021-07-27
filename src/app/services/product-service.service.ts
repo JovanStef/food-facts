6c9876dab2d1c006321bd1ed9801ad6942ffb05b
jstefanovski <jstefanovski@bitsia.com>
2021-07-27 12:45:36 +0200

    minor refactor & pagination bug fix

--- src/app/services/product-service.service.ts
+++ src/app/services/product-service.service.ts
@@ -7,7 +7,6 @@ import { Product } from '../models/Product';
 import { AlergensServiceService } from './alergens-service.service';
 import { CrudServiceService } from './crud-service.service';
 import { IngredientServiseService } from './ingredient-servise.service';
-import { of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -18,14 +17,11 @@ export class ProductServiceService {
   private maxPages: number;
   public products = new BehaviorSubject<Product[]>([])
   public ingredients:string[]=[];
-  // private _filteredProducts = new BehaviorSubject<Product[]>([])
   constructor(
     private crud:CrudServiceService , 
     private ingredientService:IngredientServiseService,
     private alergenService:AlergensServiceService
-    ) {
-
-     }
+    ) { }
 
   getProducts(): Observable<any> {
     if(this.ingredients.length !== 0){
@@ -33,35 +29,23 @@ export class ProductServiceService {
     }else{
     return this.crud.getProductsFromDB(this.page, this.show)
     .pipe(
-      map((res: any) => {
-        this.page = res.page;
-        this.show = res.page_count;
-        this.maxPages = Math.ceil(res.count / res.page_count);
-        let products = res.products.map( product => {
-
-          return this.formatProductData(product)
-        })
-        
-        return products
-      }), catchError(error => {
+      tap((res)=>this.formatPagination(res)),
+      map((res: any) => res.products.map( product => this.formatProductData(product))),
+      catchError(error => {
         console.log(error)
-        return throwError({ message: 'Something went wrong please try again later' })
-      }),tap(products => this.products.next(products))
+        return throwError({ message: 'Something went wrong please try again later' })}),
+      tap(products => this.products.next(products))
     )
   }
   }
 
   getProductById(id:string):Observable<any>{
-    return this.crud.getProductByIdFromDB(id).pipe(
-      map((res:any) =>{
-          let product = res.product;
-
-          return this.formatProductData(product)
-        
-      }), catchError(error => {
+    return this.crud.getProductByIdFromDB(id)
+    .pipe(
+      map((res:any) => this.formatProductData(res.product)), 
+      catchError(error => {
         console.log(error)
-        return throwError({ message: 'Something went wrong please try again later' })
-      })
+        return throwError({ message: 'Something went wrong please try again later' })})
     )
   }
   getFilteredProductsByIngredients(ingredients, page? ):Observable<any>{
@@ -71,17 +55,8 @@ export class ProductServiceService {
       let ingreds = this.formatIngredientsForFilter(ingredients)
       return this.crud.getProductByFilterIngredients( ingreds ,this.page, this.show)
       .pipe(
-        map((res:any) => { 
-          this.page = res.page;
-          this.show = res.page_count;
-          this.maxPages = Math.ceil(res.count / res.page_count);
-          let products = res.products.map( product => {
-            
-            return this.formatProductData(product)
-          })
-          
-          return products
-        }),
+        tap((res)=> this.formatPagination(res)),
+        map((res: any) => res.products.map( product => this.formatProductData(product))),
         catchError(error => {
           return throwError({ message: 'Something went wrong please try again later' })
         }),
@@ -127,26 +102,33 @@ export class ProductServiceService {
   formatIngredientsForFilter(ingredients){
     // tagtype_1=ingredients&tag_contains_1=contains&tag_1=mozzarella&tagtype_2=ingredients&tag_contains_2=contains&tag_2=parmesan
     let temp = ingredients.map((ingred , index)=>{
-      ingred.trim().toLowerCase()
-      ingred = ingred.trim().toLowerCase().replace(/\s/g, '%20')
+      ingred = ingred.trim().toLowerCase().replace(/\s/g, '%20');
       return`tagtype_${index+1}=ingredients&tag_contains_${index+1}=contains&tag_${index+1}=${ingred}`
     })
     return temp.join('&')
   }
+
+  // PAGINATION
+  formatPagination(res){
+    this.page = res.page;
+    this.show = res.page_count;
+    this.maxPages = Math.ceil(res.count / res.page_count);
+  }
+
   nextPage(): number {
     if (this.page <= this.maxPages) {
       this.page++
       return this.page
     } else {
-      return this.maxPages
+      return this.page = this.maxPages
     }
   }
   prevPage(): number {
-    if (this.page >= 1) {
+    if (this.page >= 2) {
       this.page--
       return this.page
     } else {
-      return 0;
+      return this.page = 1;
     }
   }
   skipForward():number {
@@ -154,20 +136,20 @@ export class ProductServiceService {
       this.page += this.skip;
       return this.page
     } else {
-      return this.maxPages
+      return this.page = this.maxPages
     }
   }
 
   skipBackward():number {
-    if (this.page + this.skip >= 1) {
+    console.log(this.page - this.skip >= 1)
+    if (this.page - this.skip >= 1) {
       this.page -= this.skip;
       return this.page
     } else {
-      return 0;
+      return this.page = 1;
     }
   }
   getPaginationData():object{
-    
     return {
       maxPages:this.maxPages,
       currPage:this.page,
